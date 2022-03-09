ffcd0f172b15e4af75ab02cd42dcfb62113485a5
jason <jluce@DESKTOP-LOTUM80.localdomain>
2022-03-08 22:31:15 -0800

    first commit

--- asm.go
+++ asm.go
@@ -9,6 +9,15 @@ import (
 	"strconv"
 )
 
+func main() {
+	labels := make(map[string]int)
+	commands := getCommands(os.Args[1], labels)
+	registers := initializeRegisters()
+	interpret(commands, registers, labels)
+	displayRegisters(registers)
+}
+
+// Initialize registers according to RISC-V specification
 func initializeRegisters() map[string]int {
 	registers := make(map[string]int)
 
@@ -49,29 +58,12 @@ func displayRegisters(registers map[string]int) {
 	fmt.Println("ra:\t", registers["ra"], "\t\tsp:\t", registers["sp"])
 	fmt.Println("-----------------------------------")
 
-	// // t0 - t6 -> temp registers
-	// for i := 0; i < 7; i++ {
-	// 	name := "t" + strconv.Itoa(i)
-	// 	fmt.Println(name + ":\t", registers[name])
-	// }
-
-	// // a0 - a6 -> function registers
-	// for i := 0; i < 7; i++ {
-	// 	name := "a" + strconv.Itoa(i)
-	// 	fmt.Println(name + ":\t", registers[name])
-	// }
-
 	for i := 0; i < 7; i++ {
 		name1 := "t" + strconv.Itoa(i)
 		name2 := "a" + strconv.Itoa(i)
 		fmt.Println(name1 + ":\t", registers[name1], "\t\t" + name2 + ":\t", registers[name2])
 	}
 
-	// s0 - s11 -> permanent registers
-	// for i := 0; i < 12; i++ {
-	// 	name := "s" + strconv.Itoa(i)
-	// 	fmt.Println(name + ":\t", registers[name])
-	// }
 	fmt.Println("-----------------------------------")
 
 	for i := 0; i < 6; i++ {
@@ -81,9 +73,9 @@ func displayRegisters(registers map[string]int) {
 	}
 }
 
-
-func getCommands(labels map[string]int) [][]string {
-	file, err := os.Open("sample.fasm")
+// Reads instructions from file into [][]
+func getCommands(filename string, labels map[string]int) [][]string {
+	file, err := os.Open(filename)
 	if err != nil {
 		log.Fatalf("failed to open")
 	}
@@ -110,19 +102,15 @@ func getCommands(labels map[string]int) [][]string {
 			}
 		}
 	}
-	 
-	// for _, t := range text {
-	// 	fmt.Println(t)
-	// }
 
 	return text
 }
 
 func interpret(text [][]string, registers map[string]int, labels map[string]int) {
-	i := labels["$main"]
+	pc := labels["$main"]
 	var stack [8096]int
-	for i < len(text) {
-		line := text[i]
+	for pc < len(text) {
+		line := text[pc]
 		switch line[0] {
 		case "li":
 			registers[line[1]], _ = strconv.Atoi(line[2])
@@ -131,41 +119,70 @@ func interpret(text [][]string, registers map[string]int, labels map[string]int)
 		case "addi":
 			num, _ := strconv.Atoi(line[3])
 			registers[line[1]] = registers[line[2]] + num
-		case "b":
-			i = labels[line[1]]
-		case "bgt":
-			left := registers[line[1]]
-			right := registers[line[2]]
-			if left > right {
-				i = labels[line[3]]
-			}
-		case "beq":
-			if registers[line[1]] == registers[line[2]] {
-				i = labels[line[3]]
-			}
+		case "b", "beq", "bgt", "ble":
+			pc = bFamilyHandler(line, registers, labels, pc)
 		case "jal":
-			registers["ra"] = i
-			i = labels[line[1]]
-			fmt.Println(labels[line[1]])
+			registers["ra"] = pc
+			pc = labels[line[1]]
 		case "ret":
-			i = registers["ra"]
+			pc = registers["ra"]
 		case "sw":
 			stack[registers[line[1]]] = registers[line[2]]
 		case "lw":
 			registers[line[1]] = stack[registers[line[2]]]
+		case "ecall":
+			eFamilyHandler(line, registers)
+		case "debug":
+			if line[1] == "stack" {
+				fmt.Println(stack[0:10])
+			} else {
+				fmt.Println(registers[line[1]])
+			}
+		case "pc":
+			fmt.Println(pc)
 		default:
 		}
-		i++
+		pc++
 	}
 }
 
-func main() {
-	labels := make(map[string]int)
-	commands := getCommands(labels)
-	registers := initializeRegisters()
-	interpret(commands, registers, labels)
-	displayRegisters(registers)
+// handles instructions of b family => comparison type instructions
+func bFamilyHandler(line []string, registers map[string]int, labels map[string]int, pc int) int {
+	switch line[0] {
+	case "b":
+		pc = labels[line[1]]
+	case "bgt":
+		left := registers[line[1]]
+		right := registers[line[2]]
+		if left > right {
+			pc = labels[line[3]]
+		}
+	case "beq":
+		if registers[line[1]] == registers[line[2]] {
+			pc = labels[line[3]]
+		}
+	case "ble":
+		if registers[line[1]] <= registers[line[2]] {
+			pc = labels[line[3]]
+		}
+	default:
+		break
+	}
+	return pc
 }
 
-// a0 - a6 t0 - t6 s0-s11 ra sp 0
-// jal lw add
+// handles special instructions
+func eFamilyHandler(line []string, registers map[string]int) {
+	switch line[1] {
+	case "1":
+		// print int in register a0
+		fmt.Println(registers["a0"])
+	case "5":
+		// read int into register a0
+		var input int
+		fmt.Scanln(&input)
+		registers["a0"] = input
+	default:
+		break
+	}
+}
